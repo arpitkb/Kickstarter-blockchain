d879af270f749dbc6f00654659dbe32179b1bed2
arpitkb <bansal_851937@student.nitw.ac.in>
2022-01-21 14:44:24 +0530

    completed ethereum code and created boilerplate for client side

---
+++ client/pages/index.js
@@ -0,0 +1,17 @@
+import MeetupList from "../components/meetups/MeetupList";
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+
+function HomePage(props) {
+  return (
+    <>
+      <Head>
+        <title>Homepage</title>
+        <meta name='description' content='Browse a huge list of meetups' />
+      </Head>
+      <div>HAHAHAHAHA</div>
+    </>
+  );
+}
+
+export default HomePage;
